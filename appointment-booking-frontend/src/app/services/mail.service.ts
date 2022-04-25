9f16694fe99a5a897e29bb86698005d05f9ddad8
Grober Balint <groberbalint@gmail.com>
2022-04-25 17:33:58 +0200

    mail service

---
+++ appointment-booking-frontend/src/app/services/mail.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Mail } from '../models/mail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MailService {
+
+  constructor(private http: HttpClient) { }
+
+  sendCreatedEmail(mail: Mail): Observable<unknown>{
+    let apiUrl = environment.apiBaseUrl + "/email/new";
+    return this.http.post(apiUrl, mail).pipe(catchError(this.handleError));
+  }
+
+  sendDeletedemail(mail: Mail): Observable<unknown>{
+    let apiUrl = environment.apiBaseUrl + "/email/deleted";
+    return this.http.post(apiUrl, mail).pipe(catchError(this.handleError));
+  }
+
+  sendEditedEmail(mail: Mail): Observable<unknown>{
+    let apiUrl = environment.apiBaseUrl + "/email/edited";
+    return this.http.post(apiUrl, mail).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+}
